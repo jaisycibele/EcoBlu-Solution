cf4bf9b6f397239206e1668d2e979d65a2b02e5b
Jaisy Cibele Alves <117952554+jaisycibele@users.noreply.github.com>
2024-06-06 22:55:45 -0300

    Delete Challenge directory

--- Challenge/challenge.zip_expanded/challenge/src/main/java/br/com/plusoft/jadv/challenge/repositorios/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.plusoft.jadv.challenge.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.plusoft.jadv.challenge.entidades.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-}
--- Challenge/challenge/src/main/java/br/com/plusoft/jadv/challenge/repositorios/CampanhaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.plusoft.jadv.challenge.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.plusoft.jadv.challenge.entidades.Campanha;
-
-public interface CampanhaRepository extends JpaRepository<Campanha, Long> {
-
-}
